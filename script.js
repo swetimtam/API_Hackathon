7af02f7d71de48754acae8018defa5c7829d8c0e
stan wielga <svwielga4@gmail.com>
2016-02-18 12:14:45 -0800

    added div

---
+++ script.js
@@ -0,0 +1,32 @@
+var tweets_array = []; //array to keep the tweets
+
+
+//function on load to pre-populate tweets
+$(document).ready(function(){
+    fetch_tweets();
+    append_tweets();
+});
+
+//function when the button is clicked
+$('button').click(function(){
+
+});
+
+//function to fetch tweets and populate the tweets_array
+//globals used: tweets_array
+function fetch_tweets(){
+    var tweets;
+    tweets_array.push(tweets);
+}
+
+//function for tweets on click to populate flickr photos
+//@parameters: first noun after the hyphen to be sent to flickr for image lookup
+
+//function to place tweets properly
+function append_tweets(){
+
+};
+
+//function to place images properly
+
+
